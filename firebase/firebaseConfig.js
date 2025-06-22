2e21bb06c754a13d5d1ea33d442ff694a8255073
morganhall1102 <phall45@gatech.edu>
2025-06-22 19:37:09 -0400

    Create firebaseConfig.js

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-DOMAIN",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-STORAGE",
+  messagingSenderId: "YOUR-SENDER-ID",
+  appId: "YOUR-APP-ID"
+};
+
+firebase.initializeApp(firebaseConfig);
